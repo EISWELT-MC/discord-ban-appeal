c9376188b42d378a0bff65932c59607067b144d5
suedpoldevelopement <68162391+suedpoldevelopement@users.noreply.github.com>
2021-09-26 16:11:12 +0200

    Update 404.js

--- src/Components/404.js
+++ src/Components/404.js
@@ -8,8 +8,8 @@ class PageNotFoundError extends Component
         return (
             <div>
                 <img alt="404 tunic" src={PageNotFound}/>
-                <h1>404 Page Not Found!</h1>
-                <h2>Looks like you went adventuring a little too far.</h2>
+                <h1>Deine Seite wurde leider nicht gefunden<h1>
+                <h2></h2>
                 <NavLink to="/">
                     Go Home
                 </NavLink>
